421649afb66e247d4141bfd3511ab109ee42cf3b
Mike Welte <mwelte1@binghamton.edu>
2013-10-09 17:49:05 -0400

    Fixed the directories so that the build.xml file works properly

---
+++ src/studentOrientation/driver/Driver.java
@@ -0,0 +1,12 @@
+//---------------------------------------------------------------------
+package studentOrientation.driver;
+//---------------------------------------------------------------------
+public class Driver 
+{
+	public static void main(String args[]) 
+	{
+		System.out.println("Driver finished executing");
+
+	} // end main(...)
+} // end class Driver
+//---------------------------------------------------------------------
---
+++ src/studentOrientation/orientation/Activity.java
@@ -0,0 +1,14 @@
+public interface Activity
+{
+	//Activity Variables
+	private int time;
+	private float cost;
+	private double effort;
+	private String name;
+
+	//Method signature declarations
+	public int getTime();
+	public float getCost();
+	public double getEffort();
+	public String getName();	
+}
