b70f6f7d727d7eeed4128e6640976f4dc2d79c4b
Mike Welte <mwelte1@binghamton.edu>
2013-10-09 18:25:29 -0400

    Worked on the concrete class

--- src/studentOrientation/orientation/UnivBookstore.java
+++ src/studentOrientation/orientation/UnivBookstore.java
@@ -1,37 +1,58 @@
-public class UnivBookstore implements Bookstore{
-    
-    //Constructor class sets values for the option
-    public UnivBookstore(){
-        UBname = "University Bookstore";
-        UBtime = //SET TIME;
-        UBcost = //SET COST;
-        UBeffot = //SET EFFORT;
-    }
-    
-    //@return name
-    public String getName(){
-        return UBname;
-    }
+public class UnivBookstore implements Bookstore
+{
 
-    //@return time
-    public int getTime(){
-        return UBtime;
-    }
-    
-    //@return cost
-    public double getCost(){
-        return UBcost;
-    }
-    
-    //@return effort
-    public int getEffort(){
-        return UBeffort;
-    }
+	//Constructor class sets values for the option
+	public UnivBookstore()
+	{
+		name = "University Bookstore";
+		/*
+		time = //SET TIME;
+		cost = //SET COST;
+		effort = //SET EFFORT;
+		*/
+	}
 
-    //To string method for the activity
-    @Override
-    public String toString() {
-        return UBname+"- Time: "+UBtime+" Cost: "+UBcost+" Calories: "+UBeffort;
-    }
+	//@return name
+	public String getName()
+	{
+		return name;
+	}
+
+	//@return time
+	public int getTime()
+	{
+		return time;
+	}
+
+	/**
+	*	This is the getter method for the cost
+	*
+	*	@return Returns the book's cost
+	**/
+	public double getCost()
+	{
+		return cost;
+	}
+    
+	/**
+	*	This is the getter met
+	*	 this class.
+	*@return 
+	**/
+	public int getEffort()
+	{
+		return effort;
+	}
+	
+	/**
+	*	This is the overridden toString method for this class
+	*	@return Returns the UnivBookstore class details as a string
+	**/
+	@Override
+	public String toString() 
+	{
+		return name+"- Time: "+time+" Cost: "+ cost 
+			+" Calories: "+UBeffort;
+	}
 }
     
