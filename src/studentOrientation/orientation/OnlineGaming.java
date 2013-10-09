541daa30357d110f0857237352f70e310e642805
pcguy5239 <aheimow1@binghamton.edu>
2013-10-09 18:44:24 -0400

    added more classes

---
+++ src/studentOrientation/orientation/OnlineGaming.java
@@ -0,0 +1,65 @@
+public class OnlineGaming implements SelectDorm
+{
+
+	//Constructor class sets values for the option
+	public OnlineGaming()
+	{
+		name = "Online Gaming Contest";
+		/*
+		time = //SET TIME;
+		cost = //SET COST;
+		effort = //SET EFFORT;
+		*/
+	}
+
+	/**
+     *	This is the getter method for the cost
+     *
+     *	@return Returns the activity's name
+     **/
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+     *	This is the getter method for the cost
+     *
+     *	@return Returns the activity's time
+     **/
+	public int getTime()
+	{
+		return time;
+	}
+
+	/**
+	*	This is the getter method for the cost
+	*
+	*	@return Returns the activity's cost
+	**/
+	public double getCost()
+	{
+		return cost;
+	}
+    
+	/**
+	*	This is the getter method for the effort
+	*@return Return the activity's effort
+	**/
+	public int getEffort()
+	{
+		return effort;
+	}
+	
+	/**
+	*	This is the overridden toString method for this class
+	*	@return Returns the class details as a string
+	**/
+	@Override
+	public String toString() 
+	{
+		return name+"- Time: "+time+" Cost: "+ cost 
+			+" Calories: "+effort;
+	}
+}
+    
