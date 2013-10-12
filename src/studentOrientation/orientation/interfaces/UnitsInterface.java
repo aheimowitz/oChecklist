cb9e35cd26c8e7f9177e26963885df44d1d2afa2
Mike <mike@Lysander.(none)>
2013-10-12 10:32:24 -0400

    Added the unit interface

---
+++ src/studentOrientation/orientation/interfaces/UnitsInterface.java
@@ -0,0 +1,9 @@
+//---------------------------------------------------------------------
+package studentOrientation.orientation.interfaces;
+//---------------------------------------------------------------------
+public interface UnitsInterface
+{
+	public void setUnits();
+	public void getUnits();
+
+}
