9b845db5984fcaeeb41cebd694853ee5237f8ba4
pcguy5239 <aheimow1@binghamton.edu>
2013-10-18 14:30:33 -0400

    builder and driver

--- src/studentOrientation/driver/Driver.java
+++ src/studentOrientation/driver/Driver.java
@@ -10,8 +10,20 @@ public class Driver
 	public static void main(String args[]) 
 	{
 		WorkshopInterface actWork = new ActivityWorkshop();
+        int debug = Integer.toInt(args[1]);
+        
+        if(debug >-1 && debug <3){
+        
+            Vector<ActivityInterface> checkList = new Vector<ActivityInterface>();
+        
+            checkList.add(new MandoBooks(debug));
+            checkList.add(new OnlineGaming(debug));
+            checkList.add(new BusToue(debug));
 		
-		System.out.println("Driver finished executing");
+            System.out.println("Driver finished executing");
+        }else{
+            System.out.println("Invalid debug value");
+        }
 
 	} // end main(...)
 } // end class Driver
