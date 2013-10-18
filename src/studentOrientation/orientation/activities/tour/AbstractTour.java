89615e6cb8994a50b1882b80e5a8f4191b81743c
Mike Welte <mwelte1@binghamton.edu>
2013-10-18 09:07:41 -0400

    Fixed the other two abstract classes

--- src/studentOrientation/orientation/activities/tour/AbstractTour.java
+++ src/studentOrientation/orientation/activities/tour/AbstractTour.java
@@ -1,6 +1,93 @@
 //---------------------------------------------------------------------
-package studentOrientation.orientation.abstractClasses;
+package studentOrientation.orientation.activities.tour;
 //---------------------------------------------------------------------
 import studentOrientation.orientation.interfaces.ActivityInterface;
 //---------------------------------------------------------------------
-public abstract class AbstractTour implements ActivityInterface{}
+public abstract class AbstractTour implements ActivityInterface
+{
+	static private final double TVALUE = 200;
+	String name;
+	int time;
+	double cost;
+	int effort;
+
+	/**
+	*
+	**/
+	public void calculate(String percent)
+	{
+
+	}
+
+	/**
+	*	This is the getter method for the name
+	*	@return Returns the activity's name
+	**/
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	*	This is the getter method for the time
+	*	@return Returns the activity's time
+	**/
+	public int getTime()
+	{
+		return time;
+	}
+
+	/**
+	*	This is the getter method for the cost
+	*	@return Returns the activity's cost
+	**/
+	public double getCost()
+	{
+		return cost;
+	}
+    
+	/**
+	*	This is the getter method for the effort
+	*	@return Return the activity's effort
+	**/
+	public int getEffort()
+	{
+		return effort;
+	}
+	
+	/**
+	*	This is the setter method for the name 
+	*	 of the activity
+	**/
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	*	This is the setter method for the time 
+	*	 of the activity
+	**/
+	public void setTime(int time)
+	{
+		this.time = time;
+	}
+
+	/**
+	*	This is the setter method for the cost
+	*	 of the activity
+	**/
+	public void setCost(double cost)
+	{
+		this.cost = cost;
+	}
+
+	/**
+	*	This is the setter method for the effort
+	*	 of the activity
+	**/
+	public void setEffort(int effort)
+	{
+		this.effort = effort;
+	}
+}
